2adec2a78062169bef76415b2218f13ecca437b5
David Reiner <reidaa15@htlkaindorf.at>
2021-04-29 12:25:34 +0200

    Initial Commit

---
+++ Assets/Scripts/NutriValues.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NutriValues : MonoBehaviour
+{
+    public string einheit = "kcal;g;g;g;g;g;g";
+    public string values1;
+    public string values2;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        values1 = "481;23;7.9;";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
